0a967d304748352328df616008d4f6998bf434fa
Scott Helm <helm.scott@gmail.com>
2018-06-18 08:03:52 -0400

    adds generation

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-function make2dArray(cols, rows) {
+function make2DArray(cols, rows) {
     let arr = new Array(cols);
     for(let x = 0; x < cols; x++) {
       arr[x] = new Array(rows);
@@ -7,44 +7,52 @@ function make2dArray(cols, rows) {
 }
 
 let grid;
-let next;
 let cols;
 let rows;
-const resolution = 10;
+let generation = 0;
+const resolution = 20;
 
 function setup() {
-    createCanvas(600,400);
+    width = Math.ceil(document.defaultView.visualViewport.width / resolution) * resolution;
+    height = Math.ceil(document.defaultView.visualViewport.height / resolution) * resolution;
+    createCanvas(width, height);
+    frameRate(20);
     cols = width / resolution;
     rows = height / resolution;
-    grid = make2dArray(cols, rows);
 
-    for(let x = 0; x < cols; x++) {
-      for(let y = 0; y < rows; y++) {
-        grid[x][y] = floor(random(2))
+    grid = make2DArray(cols, rows);
+    for(let i = 0; i < cols; i++) {
+      for(let j = 0; j < rows; j++) {
+        grid[i][j] = floor(random(2))
       }
     }
 }
 
 function draw() {
     background(0);
+
+    generation+= 1
+
     for(let i = 0; i < cols; i++) {
         for(let j = 0; j < rows; j++) {
-            x = i * resolution
-            y = j * resolution
+            let x = i * resolution;
+            let y = j * resolution;
             if(grid[i][j] == 1){
                 fill(255);
                 stroke(0);
-                rect(x, y, resolution - 1, resolution - 1)
+                rect(x, y, resolution, resolution)
             }
         }
     }
-    let next = make2dArray(cols, rows);
+
+    let next = make2DArray(cols, rows);
 
     for(let i = 0; i < cols; i++) {
         for(let j = 0; j < rows; j++) {
             let state = grid[i][j];
             let sum = 0;
             let neighbors = countNeighbors(grid, i, j)
+
             if (state == 0 && neighbors == 3) {
                 next[i][j] = 1;
             } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
@@ -54,7 +62,7 @@ function draw() {
             }
         }
     }
-
+    drawGeneration(generation);
     grid = next;
 }
 
@@ -63,10 +71,16 @@ function countNeighbors(grid, x, y) {
     for(let i = -1; i < 2; i++) {
         for(let j = -1; j < 2; j++) {
             col = (x + i + cols) % cols;
-            row = (y + i + rows) % rows;
+            row = (y + j + rows) % rows;
             sum += grid[col][row]
         }
     }
     sum -= grid[x][y];
     return sum;
+}
+
+function drawGeneration(gen) {
+    fill(117, 255, 255);
+    textSize(20);
+    text(gen, 25, 25);
 }
